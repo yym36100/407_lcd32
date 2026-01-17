91ca7db5b88b2eed3f9e69d6afd90745c87f47c2
DESKTOP-OC6KKSA\yym <yakabmarci@freemail.hu>
2026-01-17 22:22:07 +0200

    lcd timing

--- Core/Src/fsmc.c
+++ Core/Src/fsmc.c
@@ -60,10 +60,10 @@ void MX_FSMC_Init(void)
   hsram1.Init.WriteBurst = FSMC_WRITE_BURST_DISABLE;
   hsram1.Init.PageSize = FSMC_PAGE_SIZE_NONE;
   /* Timing */
-  Timing.AddressSetupTime = 15;
+  Timing.AddressSetupTime = 2;
   Timing.AddressHoldTime = 15;
-  Timing.DataSetupTime = 255;
-  Timing.BusTurnAroundDuration = 15;
+  Timing.DataSetupTime = 8;
+  Timing.BusTurnAroundDuration = 0;
   Timing.CLKDivision = 16;
   Timing.DataLatency = 17;
   Timing.AccessMode = FSMC_ACCESS_MODE_A;
--- Drivers/ili9341/lcdio.h
+++ Drivers/ili9341/lcdio.h
@@ -2,6 +2,10 @@
 
 #include <stdint.h>
 
+#define LCD_DATA	(0x60000000)
+#define LCD_CMD		(0x60000000)
+
+
 void            LCD_IO_Init(void);
 void            LCD_IO_WriteData(uint16_t Data); 
 void            LCD_IO_WriteMultipleData(uint8_t *pData, uint32_t Size);
